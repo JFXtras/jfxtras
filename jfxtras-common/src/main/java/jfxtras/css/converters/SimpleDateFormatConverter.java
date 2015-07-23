ab0052d12ee1923be824158bac56bd50fa341ae5
Tom Eugelink <tbee@tbee.org>
2015-07-23 07:22:00 +0200

    Replaced StyleConverterImpl (Jigsaw)

--- jfxtras-common/src/main/java/jfxtras/css/converters/SimpleDateFormatConverter.java
+++ jfxtras-common/src/main/java/jfxtras/css/converters/SimpleDateFormatConverter.java
@@ -33,11 +33,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import javafx.css.ParsedValue;
+import javafx.css.StyleConverter;
 import javafx.scene.text.Font;
 
-import com.sun.javafx.css.StyleConverterImpl;
-
-public class SimpleDateFormatConverter extends StyleConverterImpl<String, DateFormat> {
+public class SimpleDateFormatConverter extends StyleConverter<String, DateFormat> {
 
     @Override
     public SimpleDateFormat convert(ParsedValue<String, DateFormat> value, Font not_used) {
