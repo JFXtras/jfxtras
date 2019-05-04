328bbd448162dfbf76ac3f60a105c21308dc34e8
Danny van Bruggen <lol@meuk.info>
2019-05-04 21:14:58 +0200

    Fixes #111

--- jfxtras-window/src/main/java/module-info.java
+++ jfxtras-window/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module jfxtras.window {
     exports jfxtras.scene.control.window;
 
+    opens jfxtras.internal.scene.control.skin.window to javafx.controls;
     requires transitive jfxtras.common;
 }
