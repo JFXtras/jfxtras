ada7a87bb8ad9efeed516b055a83be450c010547
Tom Eugelink <tbee@tbee.org>
2020-05-21 23:24:54 +0200

    add licence headers to the files

--- jfxtras-controls/src/main/java/jfxtras/internal/scene/control/fxml/LocalTimeTextFieldBuilder.java
+++ jfxtras-controls/src/main/java/jfxtras/internal/scene/control/fxml/LocalTimeTextFieldBuilder.java
@@ -1,20 +1,18 @@
 /**
- * LocalTimeTextFieldBuilder.java
- *
- * Copyright (c) 2011-2016, JFXtras
+ * Copyright (c) 2011-2020, JFXtras
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
+ *    Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
+ *    Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the organization nor the
+ *    Neither the name of the organization nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,7 +24,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package jfxtras.internal.scene.control.fxml;
 
 import java.time.format.DateTimeFormatter;
--- mvnw_cleanInstall.cmd
+++ mvnw_cleanInstall.cmd
@@ -1,3 +1,3 @@
 call java_localOverride.cmd
-call mvnw clean install -DskipTests
+call mvnw clean install -DskipTests -Dmaven.javadoc.skip=true
 pause
