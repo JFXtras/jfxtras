c229e5f238124483ed29ca6b54ebf8c6be0947e6
Tom <tbee@tbee.org>
2021-05-04 02:42:04 -0700

    PR #114

--- jfxtras-window/src/main/java/module-info.java
+++ jfxtras-window/src/main/java/module-info.java
@@ -27,5 +27,6 @@
 module jfxtras.window {
     exports jfxtras.scene.control.window;
 
+    opens jfxtras.internal.scene.control.skin.window to javafx.controls;
     requires transitive jfxtras.common;
 }
